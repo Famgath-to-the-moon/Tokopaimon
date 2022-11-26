c7a280e253d5a5dc9b032b0f7216fc7075315d58
Dewa Putra Hernanda <dewananda089@gmail.com>
2022-11-26 19:53:15 +0700

    add profil view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,5 @@
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
 <x-layout.default>
 
 <div class="container bg-primary text-light mb-3 d-flex rounded justify-content-around">
@@ -18,12 +20,12 @@
         $Previous = $halaman - 1;
         $next = $halaman + 1;
 
-        $jumlah_data = 30;
+        $jumlah_data = 26;
         $total_halaman = ceil($jumlah_data / $batas);
 
         $nomor = $halaman_awal+1;
     ?>
-    @for ($i = 0; $i < 20; $i++)
+    @for ($i = 0; $i < $batas; $i++)
         <div class="col-lg-3">
             <a class="card shadow mb-4" href="https://getbootstrap.com/docs/5.2/getting-started/introduction/" style="text-decoration:none">
                 <div class="card-header py-3">
@@ -39,7 +41,9 @@
             </a>
         </div>
     @endfor
-    <nav aria-label="..." class="col">
+</div>
+
+<nav aria-label="...">
         <ul class="pagination">
             <li class="page-item">
 				<a class="page-link" <?php if($halaman > 1){ echo "href='?halaman=$Previous'"; } ?>>Previous</a>
@@ -56,5 +60,42 @@
 			</li>
         </ul>
     </nav>
+
+<div>
+    <h1>Detail Produk</h1>
+    <div class="d-flex justify-content-between pb-3 border-bottom">
+        <img src="..." class="container border flex-fill" alt="...">
+        
+        <div class="container flex-fill">
+            <h1 class="mb-0">Ini nama Produk</h1>
+            <p class="mt-1 mb-2" style="font-size:14px"><i class="fas fa-star"></i> 5</p>
+
+            <h3 class="border-bottom mb-3 pb-2">Rp 95.000</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi quas veniam ducimus enim porro necessitatibus quidem amet quos sequi, adipisci, deserunt sint inventore, doloremque repellendus dolorum labore ab ut assumenda!</p>
+            <button type="button" class="btn btn-primary" onclick="order()" >Primary</button>
+        </div>
+    </div>
+
+    <div class="container mt-3 mb-2">
+        <h3>Rekomendasi Produk</h3>
+        <div class="row">
+            @for ($i = 0; $i < 5; $i++)
+            <div class="col sm-5">
+                <a class="card shadow mb-4" href="https://getbootstrap.com/docs/5.2/getting-started/introduction/" style="text-decoration:none">
+                    <div class="card-header py-3">
+                        <img src="..." class="card-img-top" alt="...">
+                    </div>    
+                    <div class="card-body border-top">
+                        <h5 class="card-title text-secondary" style="font-weight: bold">Judul Produk</h5>
+                        <div class="mt-1">
+                            <p class="card-text border-top pt-2 mb-3 text-secondary fw-normal"><i class="fas fa-star"></i> 5</p>
+                            <p class="text-secondary mb-0 fw-normal">Rp 2500</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            @endfor
+        </div>
+    </div>
 </div>
 </x-layout.default>
